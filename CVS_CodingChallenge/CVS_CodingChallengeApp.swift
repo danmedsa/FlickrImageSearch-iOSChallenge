1ceba96dad3a8e9118e11fc9f2968ab27f82fb4b
Daniel Medina Sada <daniel.medina.sada@hotmail.com>
2024-10-19 19:44:13 -0500

    Initial Commit

---
+++ CVS_CodingChallenge/CVS_CodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVS_CodingChallengeApp.swift
+//  CVS_CodingChallenge
+//
+//  Created by Daniel Medina Sada on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CVS_CodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CVS_CodingChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CVS_CodingChallenge
+//
+//  Created by Daniel Medina Sada on 10/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
